01b333e65b3155a1018dfa8e8b764736819b91ef
Saman <samanmahmoudian.sm@gmail.com>
2024-11-18 13:01:22 +0330

    end of first part

--- app.js
+++ app.js
@@ -1,132 +1,152 @@
 const express = require("express")
+const Sequelize = require('sequelize')
 const cors = require("cors") 
 const app = express() 
 const path = require("path")
+const { promiseHooks } = require("v8")
 const port = 1111
 app.use(express.json()) 
 app.use(express.urlencoded())
 app.use(cors())
-
- 
-
 app.listen(port , ()=>{ 
     console.log('connected...') 
 })
 
-const {Users , sequelize , Login} = require('./DB')
-const { where } = require("sequelize")
+const sequelize = new Sequelize('crud' , 'postgres' , 'Saman1384' , {
+    dialect: 'postgres',
+    host: 'localhost',
+    post: 5432,
+    underscored: true
+})
 
 let students = null
 let users = {User: 'saman' , Pass:'1234'}
-let user = {}
-let per = false
-// async function creat_table(){
-//     sequelize.sync().then(()=>{
-//         console.log('Database connected succesfuly')}).catch(err=>{
-//             console.log('Failed to connect to database')
-//         })
+let login_state = false
 
-//     reload_data()
-// }
-//     creat_table()
-
-
-    
-//     async function reload_data(){
-//         students = await Users.findAll()
-//         console.log(students)
-//         get_data()
-        
-//     }
-
-// function get_data(){
-//     console.log(students)
-//     app.get('/students' , (req , res)=>{
-//         res.send(students)
-//     })
-
-    
-//     app.get('/students/:id' , (req , res)=>{
-//         const student = students.find(x => x.Id == Number(req.params.id))
-//         if (!student) {res.status(404).send("Data is not given...")}
-//         else{res.send(student)}
-//     })
-// }
-
-
-
-//     function add_student(student){
-//         Users.create({Name:student.Name , Family:student.Family , Id:student.Id 
-//             , Date_of_birth:student.Date_of_birth , Major:student.Major 
-//             , University:student.University}).then(()=>{
-//                 console.log('Data added succesfuly')
-//             }).catch(err=>{
-//                 console.log('Failed to added data')
-//             })
-//     }
-
-
-// app.post("/students" , (req , res) => {
-
-//     const student = {
-//         Name : req.body.Name , 
-//         Family : req.body.Family,
-//         Id : req.body.Id,
-//         Date_of_birth : req.body.Date_of_birth,
-//         Major : req.body.Major,
-//         University : req.body.University
-//     }
-//     add_student(student)
-//     students.push(student)
-    
-//     res.send(student)
-   
-
-    
-// })
-// app.put("/students/:id" , (req , res) => {
-//     const student = students.find(x => x.Id == Number(req.params.id))
-//     if (!student) {res.status(404).send("Data is not given...")}
-//     else{
-
-//             student.Name = req.body.Name,
-//             student.Family = req.body.Family,
-//             student.Id = req.body.Id,
-//             student.Date_of_birth = req.body.Date_of_birth,
-//             student.Major = req.body.Major,
-//             student.Universityni = req.body.University
-//     }
-//     Users.update({Name:student.Name , Family:student.Family
-//         , Date_of_birth:student.Date_of_birth , Major:student.Major 
-//         , University:student.University} , {where : {Id : student.Id}}).then(()=>{
-//             console.log('Data updated succesfuly')
-//         }).catch(err=>{
-//             console.log('Failed to update data')
-//         })
-// })
-// app.delete("/students/:id" , (req , res) => {
-//     const student = students.find(x => x.Id == Number(req.params.id))
-//     Users.destroy({where : {Id : student.Id}})
-  
-//     students.splice(students.indexOf(student) , 1)
-//     console.log("Data deleted succesfully...")
-// })
-app.post('/login' , async (req , res)=>{
-    user = await {
-        User: req.body.User,
-        Pass: req.body.Pass
-    }
-    
-})
-app.get("/login" , async (req , res)=>{
-    if(user.User){
-        if(user.User == users.User && user.Pass == users.Pass){
-            await res.send(user.User)
+let pr = new Promise((resolve,reject)=>{
+    app.post('/login_check' , (req,res)=>{
+        if (req.body.User == users.User && req.body.Pass == users.Pass){
+            login_state = req.body.User
+            app.get('/login_status' , (req,res)=>{
+                res.send('done')
+            })
+            const table = sequelize.define(`${login_state}_tables`, {
+                Name:{
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+                Family:{
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+                Id:{
+                    type: Sequelize.INTEGER,
+                    primaryKey: true,
+                    allowNull: false
+                },
+                Date_of_birth:{
+                    type: Sequelize.DATEONLY,
+                    allowNull: false
+                },
+                Major:{
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+                University:{
+                    type: Sequelize.STRING,
+                    allowNull: false
+                }
+                
+            })
+            
+            table.sync().then(()=>{
+                console.log('Database connected succesfuly')}).catch(err=>{
+                    console.log('Failed to connect to database')
+                })
+            resolve(table)
         }else{
-           await res.send('error')
+            app.get('/login_status' , (req,res)=>{
+                res.send('failed')
+            })
+            reject()
         }
-    }else{
-        res.send('EnterData')
+    })
+}).then(async (table)=>{
+    app.get('/students' , async(req , res)=>{
+        students =await table.findAll()
+        res.send(students)
+    })
+    app.get('/students/:id' ,async (req , res)=>{
+        const student = await students.find(x => x.Id == Number(req.params.id))
+        if (!student) {res.status(404).send("Data is not given...")}
+        else{res.send(student)}
+    })
+    function add_student(student){
+        table.create({Name:student.Name , Family:student.Family , Id:student.Id 
+            , Date_of_birth:student.Date_of_birth , Major:student.Major 
+            , University:student.University}).then(()=>{
+                console.log('Data added succesfuly')
+            }).catch(err=>{
+                console.log('Failed to added data')
+            })
     }
+    
+    app.post("/students" , async(req , res) => {
+        await new Promise((resolve)=>{
+            const student = {
+                Name : req.body.Name , 
+                Family : req.body.Family,
+                Id : req.body.Id,
+                Date_of_birth : req.body.Date_of_birth,
+                Major : req.body.Major,
+                University : req.body.University
+            }
+            resolve(student)
+        }).then((student)=>{
+            add_student(student)
+            students.push(student)
+            res.send(student)
+        })
+
+    })
+    app.delete("/students/:id" , async (req , res) => {
+        const student = await students.find(x => x.Id == Number(req.params.id))
+        table.destroy({where : {Id : student.Id}})
+        students.splice(students.indexOf(student) , 1)
+        console.log("Data deleted succesfully...")
+        res.send('done')
+        
+    })
+    app.put("/students/:id" , async(req , res) => {
+        const student = await students.find(x => x.Id == Number(req.params.id))
+        let request = new Promise((resolve)=>{
+            if (!student) {res.status(404).send("Data is not given...")}
+            else{
+                student.Name = req.body.Name,
+                student.Family = req.body.Family,
+                student.Id = req.body.Id,
+                student.Date_of_birth = req.body.Date_of_birth,
+                student.Major = req.body.Major,
+                student.University = req.body.University
+            }
+            resolve(student)
+        }).then((student)=>{
+            table.update({Name:student.Name , Family:student.Family
+            , Date_of_birth:student.Date_of_birth , Major:student.Major 
+            , University:student.University} , {where : {Id : student.Id}}).then(()=>{
+            console.log('Data updated succesfuly')
+            students = table.findAll() 
+            res.send('done')
+            }).catch(err=>{
+                console.log('Failed to update data')
+            })   
+        })
+
+    })
+
 
 })
+
+
+
+
